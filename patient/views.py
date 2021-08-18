3e732e8fd253d0ede681e4dd0ef54b8880de8ba9
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-08-18 09:37:46 +0200

    select all patients

--- patient/views.py
+++ patient/views.py
@@ -15,7 +15,8 @@ def list_patients_informations(request):
 
 @api_view(['GET', ])
 def list_patients_orm(request):
-     patients = Patient.objects.all().order_by('id')[:100]
+     patients = Patient.objects.all()
      patient_serializer = PatientSerializer(patients , many=True)
      data = {"response id": '0' ,"data": patient_serializer.data}
      return Response(data)
+     
